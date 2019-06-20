5a9785e0f3717d0d8d93ff6294dd4f0f9f40559e
Ghulam Mustafa <ghulam.mustafa@webware.om>
2019-06-20 09:51:05 +0400

    This is the init of this repository

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App for Map',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter App for Map Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This app is for demonstrating the map and the selected location.'
+            ),
+            FlatButton(
+              child: Text('Ask for permission'),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text('Show me maps'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
